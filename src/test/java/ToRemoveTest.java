9f2f9ebde693a526deb6077e192a1cb1daaeb509
emaksimova <onaxiz@tutanota.com>
2021-04-10 17:41:11 +0300

    add data providers

--- src/test/java/ToRemoveTest.java
+++ src/test/java/ToRemoveTest.java
@@ -1,13 +1,13 @@
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-
 import java.lang.reflect.Method;
 import java.time.LocalDate;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.testng.Assert.assertEquals;
 
 public class ToRemoveTest extends ToRemove implements TestPriority {
-
 
     @BeforeMethod
     public void displayingTestDescription(Method testMethod){
@@ -17,38 +17,62 @@ public class ToRemoveTest extends ToRemove implements TestPriority {
 
     @Test(description = "verify that string is in UpperCase", priority = LOW)
     public void testTestToUpperCase() {
-        String testString = "test";
+        final String testString = "test";
         String actualResult = toUpperCase(testString);
-        String expectedResult = "TEST";
+        final String expectedResult = "TEST";
         final String message = "The text of the line feed to uppercase failed: ";
 
         assertEquals(actualResult, expectedResult, message);
     }
 
-
-    @Test(description = "checking multiplication result", priority = HIGH)
-    public void testTestMultiply() {
-        int a = 2;
-        long b = 3_000_000L;
-        double actualResult = multiply(a, b);
-        double expectedResult = 6_000_000L;
-        final String message = "Multiply failed: ";
-        assertEquals(actualResult, expectedResult, message);
+    @DataProvider(name = "multiplyData")
+    public Object[][] dataForMultiply() {
+        return new Object[][]{
+                {2, 3_000_000_000L, 6_000_000_000D},
+                {-2, -3_000_000_000L, 6_000_000_000D},
+                {-2,  3_000_000_000L, -6_000_000_000D},
+                { 2,  -3_000_000_000L, -6_000_000_000D},
+                { 0,  -3_000_000_000L, 0},
+                { 2, 0, 0},
+                { 2_147_483_400L,1, 2_147_483_400L},
+                {2_147_483_647, 9_223_372_036_854_775_807L, 19_807_040_619_342_712_359_383_728_129D}
+        };
     }
 
-    @Test(description = "verify DESC order", priority = MEDIUM)
-    public void testTestSortDesc() {
-        int[] testArray = {144, 30, 30, 34, 36, 144, 39, 45, -21, 89};
-        int[] expectedArray = {144, 144, 89, 45, 39, 36, 34, 30, 30, -21};
+    @Test(dataProvider = "multiplyData", priority = HIGH, description = "testMultiply")
+    public void testTMultiply(int a,long b, double expected) {
+        double actualResult = multiply(a, b);;
+        final String message = "Multiply failed: ";
+        assertEquals(actualResult, expected, message);
+    }
+
+    @DataProvider(name = "DESC")
+    public Object[][] dataForDESC() {
+        return new Object[][]{
+                { new int[]{2, 4, 3, 1}, new int[]{4, 3, 2, 1}},
+                { new int[]{2, 0, 2, 0}, new int[]{2, 2, 0, 0}},
+                { new int[]{2, -4, 3, -1}, new int[]{3, 2, -1, -4}}
+        };
+    }
+
+    @Test(dataProvider = "DESC", description = "verify DESC order", priority = MEDIUM)
+    public void testTestSortDesc(int[] testArray, int[] expectedArray) {
         final String message = "DESC sorting failed: ";
         sortDesc(testArray);
         assertEquals(testArray, expectedArray, message);
     }
 
-    @Test(description = "verify ASC order", priority = MEDIUM)
-    public void testTestSortAsc() {
-        int[] testArray = {144, 30, 30, 34, 36, 144, 39, 45, -21, 89};
-        int[] expectedArray = {-21, 30, 30, 34, 36, 39, 45, 89, 144, 144};
+    @DataProvider(name = "ASC")
+    public Object[][] dataForASC() {
+        return new Object[][]{
+                { new int[]{2, 4, 3, 1}, new int[]{1, 2, 3, 4}},
+                { new int[]{2, 0, 2, 0}, new int[]{0, 0, 2, 2}},
+                { new int[]{2, -4, 3, -1}, new int[]{-4, -1, 2, 3}}
+        };
+    }
+
+    @Test(dataProvider = "ASC", description = "verify ASC order", priority = MEDIUM)
+    public void testTestSortAsc(int[] testArray, int[] expectedArray) {
         final String message = "ASC sorting failed: ";
         sortDesc(testArray);
         assertEquals(testArray, expectedArray, message);
@@ -65,21 +89,23 @@ public class ToRemoveTest extends ToRemove implements TestPriority {
 
     @Test(description = "verify casting to PersonDto class object ", priority = HIGH)
     public void testToPersonDto() {
-        ToRemove.Person testPerson = new ToRemove.Person(1L, "a", "b", "fd", LocalDate.now());
+        ToRemove.Person testPerson = new ToRemove.Person(1L, "a", "b", "fd", LocalDate.parse("2021-04-10"));
         ToRemove.PersonDto actualPersonDto = toPersonDto(testPerson);
         ToRemove.PersonDto expectedPersonDto = new ToRemove.PersonDto(Long.valueOf(1L).toString(), "a", "b", "fd",
-                LocalDate.now().toString());
+                "2021-04-10");
         final String message = "toPersonDto method failed: ";
-        assertEquals(actualPersonDto, expectedPersonDto, message);
+        assertThat(actualPersonDto)
+                .isEqualToComparingFieldByFieldRecursively(expectedPersonDto);
     }
 
     @Test(description = "verify casting to Person class object ", priority = HIGH)
     public void testToPerson()  {
         ToRemove.PersonDto personDto = new ToRemove.PersonDto(Long.valueOf(1L).toString(), "a", "b", "fd",
-                LocalDate.now().toString());
+                "2021-04-10");
         ToRemove.Person actualPerson = toPerson(personDto);
-        ToRemove.Person expectedPerson = new ToRemove.Person(1L, "a", "b", "fd", LocalDate.now());
+        ToRemove.Person expectedPerson = new ToRemove.Person(1L, "a", "b", "fd", LocalDate.parse("2021-04-10"));
         final String message = "toPerson method failed: ";
-        assertEquals(actualPerson, expectedPerson, message);
+        assertThat(actualPerson)
+                .isEqualToComparingFieldByFieldRecursively(expectedPerson);
     }
 }
